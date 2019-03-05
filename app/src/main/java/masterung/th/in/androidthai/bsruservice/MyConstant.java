32edc9ca6a1e7fe6362a2bbd735c1fe0415d0b96
beemsaber <beemsaber@hotmail.com>
2019-03-05 11:46:30 +0700

    Initial commit

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,17 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/bsru/addUserTong.php";
+    private String urlGetUserWhereUser = "https://androidthai.in.th/bsru/getUserWhereUserTong.php";
+
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
+}
